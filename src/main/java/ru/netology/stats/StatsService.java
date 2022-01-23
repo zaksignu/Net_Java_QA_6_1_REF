c2a1ef1b4f98fca6683ddb4a0e8e6657e902973c
zaksignu <zaksignu@gmail.com>
2022-01-23 11:50:01 +0300

    3d commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,17 +1,69 @@
 package ru.netology.stats;
 
 public class StatsService {
+
+    //1. Сумма всех продаж
+    public int wholeSalesSumm(long[] sales) {
+        int summ = 0;
+        for (long sale : sales) {
+            summ += sale;
+        }
+        return summ;
+    }
+
+    //2. Средняя сумма продаж в месяц
+    public int averageSalesSumm(long[] sales) {
+        int summ = 0;
+        for (long sale : sales) {
+            summ += sale;
+        }
+        summ = summ / sales.length;
+        return summ;
+    }
+
+    //3.Номер месяца, в котором был минимум продаж (осуществлены продажи на минимальную сумму)*
     public int minSales(long[] sales) {
         int minMonth = 0;
-        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        int month = 0;
         for (long sale : sales) {
-            // sales[minMonth] - продажи в месяце minMonth
-            // sale - продажи в рассматриваемом месяце
             if (sale <= sales[minMonth]) {
                 minMonth = month;
             }
-            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+            month = month + 1;
         }
         return minMonth + 1;
+    }
+
+    //4.Номер месяца, в котором был максимум продаж (осуществлены продажи на минимальную сумму)*
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int moreAverageSales(long[] sales, long avrgSumm) {
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            if (sale > avrgSumm) {
+                month++;
+            }
+        }
+        return month;
+    }
+
+    public int lessAverageSales(long[] sales, long avrgSumm) {
+        int month = 0;
+        for (long sale : sales) {
+            if (sale < avrgSumm) {
+                month++;
+            }
+        }
+        return month;
     }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -8,11 +8,57 @@ class StatsServiceTest {
 
     @Test
     void minSales() {
-        int expected = 1;
+        int expected = 9;
         StatsService srv = new StatsService();
-        long[] time = {1,2,3};
+        long[] salesInTime = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = srv.minSales(salesInTime);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void wholeSalesSumm() {
+        int expected = 180;
+        StatsService srv = new StatsService();
+        long[] salesInTime = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = srv.wholeSalesSumm(salesInTime);
+        assertEquals(expected, actual);
+    }
 
-        assertEquals(expected,srv.minSales(time));
+    @Test
+    void averageSalesSumm() {
+        int expected = 15;
+        StatsService srv = new StatsService();
+        long[] salesInTime = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = srv.averageSalesSumm(salesInTime);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxSales() {
+        int expected = 8;
+        StatsService srv = new StatsService();
+        long[] salesInTime = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = srv.maxSales(salesInTime);
+        assertEquals(expected, srv.maxSales(salesInTime));
+    }
+
+    @Test
+    void moreAverageSales() {
+        int expected = 5;
+        StatsService srv = new StatsService();
+        long[] salesInTime = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long avrgSumSales = srv.averageSalesSumm(salesInTime);
+        int actual = srv.moreAverageSales(salesInTime, avrgSumSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void lessAverageSales() {
+        int expected = 5;
+        StatsService srv = new StatsService();
+        long[] salesInTime = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long avrgSumSales = srv.averageSalesSumm(salesInTime);
+        int actual = srv.lessAverageSales(salesInTime, avrgSumSales);
+        assertEquals(expected, actual);
     }
 }
